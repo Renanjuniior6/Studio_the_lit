5e15a7921cfecb899a14b05741f42cdaa62a5e13
Renanjuniior6 <renanjrr679@gmail.com>
2025-07-24 17:13:05 -0300

    fix: remocao dos espaços em branco

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,8 +37,8 @@ export default function About() {
   }, []);
 
 
-    return (
-        <section className={styles.section}>
+  return (
+      <section className={styles.section}>
 
             {imagens.map((src, index) => (
                 <Image
@@ -60,14 +60,14 @@ export default function About() {
             />
           ))}
 
-              <div className={styles.about}>
-                  <h5>SOBRE</h5>
-                  <h2>
-                      O Estúdio TK é um espaço seguro e inclusivo onde estilos de tatuagem globais ganham vida. 
-                      Nossos talentosos artistas internacionais se dedicam a transformar sua visão em uma obra-prima duradoura.
-                  </h2>
-              </div>
+            <div className={styles.about}>
+                <h5>SOBRE</h5>
+                <h2>
+                    O Estúdio TK é um espaço seguro e inclusivo onde estilos de tatuagem globais ganham vida. 
+                    Nossos talentosos artistas internacionais se dedicam a transformar sua visão em uma obra-prima duradoura.
+                </h2>
+            </div>
          
-        </section>
+      </section>
     )
 }
