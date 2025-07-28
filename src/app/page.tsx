b3af2e2bef550e9d9d1a02490d27a181cc3880dc
Renan <renanjrr679@gmail.com>
2025-07-28 10:57:51 -0300

    style: correções

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import styles from "../styles/page.module.css";
 export default function Home() {
   return (
     <main className={styles.page}>
-      <Hero />
-      <About />
-      <Assessments />
+        <Hero />
+        <About />
+        <Assessments />
     </main>
   );
 }
