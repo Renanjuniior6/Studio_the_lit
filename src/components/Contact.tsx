360095dbc455ce3f5252100583715bef41c8c9cc
Renan <renanjrr679@gmail.com>
2025-08-06 21:21:17 -0300

    feat: estrutura de contato

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -16,6 +16,38 @@ export default function Contact() {
                     <button>Entre em Contato</button>
                 </div>
             </div>
+            <div className={styles.contact_info}>
+                <p className={styles.contact_title}>MARQUE CONOSCO</p>
+                <div className={styles.box_contact_1}>
+                    <div>
+                        <p>MON - FRI.</p>
+                        <p>9:00 AM – 5:00 PM</p>
+                        <br/>
+                        <p>SATURDAY.</p>
+                        <p>9:00 AM – 2:00 PM</p>
+                        <br/>
+                    </div>
+
+                    <div>
+                        <p>SUN</p>
+                        <p>9:00 AM – 5:00 PM</p>
+                        <br/>
+                        <p>HOLIDAY.</p>
+                        <p>9:00 AM – 2:00 PM</p>
+                    </div>
+                </div>
+                <div className={styles.box_contact_2}>
+                    <p>
+                        500 TERRY FRANCINE ST.
+                        SAN FRANCISCO, CA 94158
+                    </p>
+                    <iframe></iframe>
+                </div>
+                <div className={styles.box_contact_3}>
+                    <p>@instaStudioTk</p>
+                    <p>Tel.: (21) 98765-8976</p>
+                </div>
+            </div>
       </section>
     )
 }
